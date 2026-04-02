d082817f1543098d1aaed2acefad4ff8974460fd
Alexis Romero Mendoza <alexis.rinnegan@gmail.com>
2026-04-01 22:36:38 -0600

    Update mejoravit.schema.ts

--- src/features/mejoravit/schemas/mejoravit.schema.ts
+++ src/features/mejoravit/schemas/mejoravit.schema.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod';
 
 export const calculatorSchema = z.object({
-    valorMejora: z.number({ invalid_type_error: 'Debe ser un número' }).positive('El valor debe ser mayor a 0'),
+    valorMejora: z.number({
+        message: "El valor es requerido y debe ser un número"
+    }).positive('El valor debe ser mayor a 0'),
 
-    plazo: z.number().int().min(1, 'Plazo mínimo: 1 año').max(30, 'Plazo máximo: 30 años'),
+    plazo: z.number({
+        message: "El plazo es requerido y debe ser un número"
+    }).int().min(1, 'Plazo mínimo: 1 año').max(30, 'Plazo máximo: 30 años'),
 
-    requiereRegularizacion: z.boolean(),
+    requiereRegularizacion: z.boolean({
+        message: "Debe seleccionar una opción"
+    }),
 })
 
 export type CalculatorSchema = z.infer<typeof calculatorSchema>;
