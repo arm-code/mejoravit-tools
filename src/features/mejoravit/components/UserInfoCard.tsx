b4c4a948b8fc7a107bba3b30805f230c522cbfc7
Alexis Romero Mendoza <alexis.rinnegan@gmail.com>
2026-04-01 21:59:51 -0600

    feat: adding user card

--- src/features/mejoravit/components/UserInfoCard.tsx
+++ src/features/mejoravit/components/UserInfoCard.tsx
@@ -1,9 +1,15 @@
+import { useMejoravit } from "../hooks/useMejoravit";
 
 
 export function UserInfoCard() {
+    const { user } = useMejoravit();
     return (
-        <div>
-            <h2>Información del usuario</h2>
+        <div className=" display flex justify-start items-start flex-col w-full">
+
+            <p className="text-black font-bold">N.S.S.: {user.nss}</p>
+            <p className="text-black font-bold">R.F.C.: {user.rfc}</p>
+            <p className="text-black font-bold">Nombre: {user.nombre}</p>
+
         </div>
     );
 }
